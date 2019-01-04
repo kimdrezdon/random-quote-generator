f4e0d148ac67643d432db62c8ff1bde1d845fd5c
Kim Drezdon <kdrezdon@gmail.com>
2019-01-03 19:52:12 -0600

    Minor syntax changes

--- js/script.js
+++ js/script.js
@@ -69,32 +69,32 @@ let quotes = [
 ];
 
 /***
-Creates a random RGB number string by generating 3 random numbers. 
-Applies that random RGB color string to the background color. 
+Creates a random RGB number string by generating 3 random numbers.
+Applies that random RGB color string to the background color.
 Colors are limited to darker colors to prevent blending with the white font.
 ***/
 
 function randomRGB() {
     return Math.floor(Math.random() * 180);
-};
+}
 
 function randomColor() {
-    var color = "rgb(";
+    let color = "rgb(";
     color += randomRGB() + ",";
     color += randomRGB() + ",";
     color += randomRGB() + ")";
     document.body.style.backgroundColor = color;
-};
+}
 
 //Generates a random number and returns the quote object with that index number from the array.
 
 function getRandomQuote(array) {
     let randomNumber = Math.floor(Math.random() * array.length);
-    return array[randomNumber]
-};
+    return array[randomNumber];
+}
 
 /***
-Calls the getRandomQuote function to get a random quote object from the quotes array. 
+Calls the getRandomQuote function to get a random quote object from the quotes array.
 Builds an HTML string using the properties of the quote object, only if they exist.
 Removes the contents of the 'quote-box' div element and replaces with the new HTML string.
 Calls the randomColor function to randomly change the background color.
@@ -108,26 +108,26 @@ function printQuote() {
     quoteString += "<p class='source'>" + randomQuote.source ;
     if (randomQuote.citation) {
         quoteString += "<span class='citation'>" + randomQuote.citation + "</span>";
-    };
+    }
     if (randomQuote.year) {
         quoteString += "<span class='year'>" + randomQuote.year + "</span>";
-    };
-    quoteString += "</p>"
+    }
+    quoteString += "</p>";
     if (randomQuote.tags) {
         quoteString += "<p class='tags'>" + randomQuote.tags + "</p>";
-    };
+    }
     document.getElementById("quote-box").innerHTML = quoteString;
     randomColor();
     clearInterval(intervalID);
     autoRefresh();
-};
+}
 
-//Auto refreshes the quote and background color by calling the printQuote function every 20 seconds.
+//Auto refreshes the quote and background color by calling the printQuote function every 15 seconds.
 
 function autoRefresh() {
-    intervalID = setInterval(printQuote, 20000);
-};
+    intervalID = setInterval(printQuote, 15000);
+}
 
 //Calls the printQuote function each time the 'Show Another Quote' button is clicked.
 
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+document.getElementById("loadQuote").addEventListener("click", printQuote, false);
