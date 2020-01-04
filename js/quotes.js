b4e5ddadec611a4316ebf33e85170abe3fdeff00
Kim Drezdon <kdrezdon@gmail.com>
2020-01-04 13:04:01 -0600

    Moved quotes array to separate js file

---
+++ js/quotes.js
@@ -0,0 +1,73 @@
+"use strict";
+
+//Creates an array of quote objects to contain the quote properties that will be displayed on the page.
+
+let quotes = [
+    {
+        id: 0,
+        quote: "There’s nothing we can’t do if we work hard, never sleep, and shirk all other responsibilities in our lives.",
+        source: "Leslie Knope",
+        citation: "Parks and Recreation",
+        tags: "humor, motivational, hard work"
+    },
+    {
+        id: 1,
+        quote: "I knew exactly what to do. But in a much more real sense, I had no idea what to do.",
+        source: "Michael Scott",
+        citation: "The Office",
+        year: 2009,
+        tags: "humor, problem-solving"
+    },
+    {
+        id: 2,
+        quote: "There is literally nothing in this world that you cannot do.",
+        source: "Chris Traeger",
+        citation: "Parks and Recreation",
+        tags: "motivational, hard work"
+    },
+    {
+        id: 3,
+        quote: "Pretty good. Pretty, pretty, pretty, pretty good.",
+        source: "Larry David",
+        citation: "Curb Your Enthusiasm",
+        tags: "humor, positivity"
+    },
+    {
+        id: 4,
+        quote: "It's not magic. It's talent and sweat.",
+        source: "Gilfoyle",
+        citation: "Silicon Valley",
+        year: 2014,
+        tags: "hard work, motivational"
+    },
+    {
+        id: 5,
+        quote: "If you're having a bad day, just look in the mirror and say: How you doin?",
+        source: "Joey Tribbiani",
+        citation: "Friends",
+        tags: "humor, hope, positivity"
+    },
+    {
+        id: 6,
+        quote: "You are living an exciting new chapter in the book on tape of your life.",
+        source: "Moira Rose",
+        citation: "Schitt'$ Creek",
+        tags: "humor, change, hope"
+    },
+    {
+        id: 7,
+        quote: "Sometimes when I start a sentence, I don't even know where it's going, I just hope I find it along the way.",
+        source: "Michael Scott",
+        citation: "The Office",
+        year: 2009,
+        tags: "humor, hope"
+    },
+    {
+        id: 8,
+        quote: "I've often felt that dreams are answers to questions we haven't yet figured out how to ask.",
+        source: "Fox Mulder",
+        citation: "The X-Files",
+        year: 1995,
+        tags: "dreams, problem-solving"
+    }
+  ];
