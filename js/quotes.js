15896f387490529591c7880753dff0cec664f975
Kim Drezdon <kdrezdon@gmail.com>
2020-04-23 16:14:54 -0500

    Apply prettier changes

--- js/quotes.js
+++ js/quotes.js
@@ -1,64 +1,70 @@
-"use strict";
+'use strict';
 
 //Creates an array of quote objects to contain the quote properties that will be displayed on the page.
 
-let quotes = [
+const quotes = [
     {
-        quote: "There’s nothing we can’t do if we work hard, never sleep, and shirk all other responsibilities in our lives.",
-        source: "Leslie Knope",
-        citation: "Parks and Recreation",
-        tags: "humor, motivational, hard work"
+        quote:
+            'There’s nothing we can’t do if we work hard, never sleep, and shirk all other responsibilities in our lives.',
+        source: 'Leslie Knope',
+        citation: 'Parks and Recreation',
+        tags: 'humor, motivational, hard work',
     },
     {
-        quote: "I knew exactly what to do. But in a much more real sense, I had no idea what to do.",
-        source: "Michael Scott",
-        citation: "The Office",
+        quote:
+            'I knew exactly what to do. But in a much more real sense, I had no idea what to do.',
+        source: 'Michael Scott',
+        citation: 'The Office',
         year: 2009,
-        tags: "humor, problem-solving"
+        tags: 'humor, problem-solving',
     },
     {
-        quote: "There is literally nothing in this world that you cannot do.",
-        source: "Chris Traeger",
-        citation: "Parks and Recreation",
-        tags: "motivational, hard work"
+        quote: 'There is literally nothing in this world that you cannot do.',
+        source: 'Chris Traeger',
+        citation: 'Parks and Recreation',
+        tags: 'motivational, hard work',
     },
     {
-        quote: "Pretty good. Pretty, pretty, pretty, pretty good.",
-        source: "Larry David",
-        citation: "Curb Your Enthusiasm",
-        tags: "humor, positivity"
+        quote: 'Pretty good. Pretty, pretty, pretty, pretty good.',
+        source: 'Larry David',
+        citation: 'Curb Your Enthusiasm',
+        tags: 'humor, positivity',
     },
     {
         quote: "It's not magic. It's talent and sweat.",
-        source: "Gilfoyle",
-        citation: "Silicon Valley",
+        source: 'Gilfoyle',
+        citation: 'Silicon Valley',
         year: 2014,
-        tags: "hard work, motivational"
+        tags: 'hard work, motivational',
     },
     {
-        quote: "If you're having a bad day, just look in the mirror and say: How you doin?",
-        source: "Joey Tribbiani",
-        citation: "Friends",
-        tags: "humor, hope, positivity"
+        quote:
+            "If you're having a bad day, just look in the mirror and say: How you doin?",
+        source: 'Joey Tribbiani',
+        citation: 'Friends',
+        tags: 'humor, hope, positivity',
     },
     {
-        quote: "You are living an exciting new chapter in the book on tape of your life.",
-        source: "Moira Rose",
+        quote:
+            'You are living an exciting new chapter in the book on tape of your life.',
+        source: 'Moira Rose',
         citation: "Schitt'$ Creek",
-        tags: "humor, change, hope"
+        tags: 'humor, change, hope',
     },
     {
-        quote: "Sometimes when I start a sentence, I don't even know where it's going, I just hope I find it along the way.",
-        source: "Michael Scott",
-        citation: "The Office",
+        quote:
+            "Sometimes when I start a sentence, I don't even know where it's going, I just hope I find it along the way.",
+        source: 'Michael Scott',
+        citation: 'The Office',
         year: 2009,
-        tags: "humor, hope"
+        tags: 'humor, hope',
     },
     {
-        quote: "I've often felt that dreams are answers to questions we haven't yet figured out how to ask.",
-        source: "Fox Mulder",
-        citation: "The X-Files",
+        quote:
+            "I've often felt that dreams are answers to questions we haven't yet figured out how to ask.",
+        source: 'Fox Mulder',
+        citation: 'The X-Files',
         year: 1995,
-        tags: "dreams, problem-solving"
-    }
-  ];
+        tags: 'dreams, problem-solving',
+    },
+];
